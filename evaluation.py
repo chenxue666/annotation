9d7ccccc66042e721e8b0cb69e73a21307a070e0
chenxue <xuechen789@hotmail.com>
2018-07-06 22:08:49 -0400

    fix the missing data

--- evaluation.py
+++ evaluation.py
@@ -58,64 +58,64 @@ with open(home + "/coding/annotation/result/result.csv", "w") as csvfile:
         for idx, item in enumerate(machine_keywords):
             print(idx, item.word)
 
-        # plot k-line
-        df_price = []
-        df_price_dict = []
-        fig, ax = plt.subplots(figsize=(8, 5))
-        
-        time = "2018-04-02 12:45:33"
+        time = "2018-04-12 23:00:00"
+        if time < "2018-04-14":
+            # plot k-line
+            df_price = []
+            df_price_dict = []
+            fig, ax = plt.subplots(figsize=(8, 5))
 
-        time_window = [
-            (datetime.strptime(time, "%Y-%m-%d %H:%M:%S") + timedelta(hours=-2)).strftime("%Y-%m-%dT%H:%M:%S"),
-            (datetime.strptime(time, "%Y-%m-%d %H:%M:%S") + timedelta(hours=5)).strftime("%Y-%m-%dT%H:%M:%S"),
-        ]
+            time_window = [
+                (datetime.strptime(time, "%Y-%m-%d %H:%M:%S") + timedelta(hours=-2)).strftime("%Y-%m-%dT%H:%M:%S"),
+                (datetime.strptime(time, "%Y-%m-%d %H:%M:%S") + timedelta(hours=5)).strftime("%Y-%m-%dT%H:%M:%S"),
+            ]
 
-        for endpoint in price_json:
-            start_time = endpoint["time_period_start"][:19]
-            end_time = endpoint["time_period_end"][:19]
-            if start_time >= time_window[0] and start_time <= time_window[1] and end_time >= time_window[0] and end_time <= time_window[1]:
-                date_time_start = datetime.strptime(endpoint["time_period_start"][:19], "%Y-%m-%dT%H:%M:%S")
-                date_time_end = datetime.strptime(endpoint["time_period_end"][:19], "%Y-%m-%dT%H:%M:%S")
-                mid_point = date_time_start + timedelta(seconds=30)
-                df_tmp = [
-                    date_time_start,
-                    date_time_end,
-                    endpoint["price_close"],
-                    endpoint["price_high"],
-                    endpoint["price_low"],
-                    endpoint["price_open"],
-                ]
-                df_price.append(df_tmp)
-                df_price_dict.append([mid_point, np.mean([endpoint["price_high"], endpoint["price_low"]])])
+            for endpoint in price_json:
+                start_time = endpoint["time_period_start"][:19]
+                end_time = endpoint["time_period_end"][:19]
+                if start_time >= time_window[0] and start_time <= time_window[1] and end_time >= time_window[0] and end_time <= time_window[1]:
+                    date_time_start = datetime.strptime(endpoint["time_period_start"][:19], "%Y-%m-%dT%H:%M:%S")
+                    date_time_end = datetime.strptime(endpoint["time_period_end"][:19], "%Y-%m-%dT%H:%M:%S")
+                    mid_point = date_time_start + timedelta(seconds=30)
+                    df_tmp = [
+                        date_time_start,
+                        date_time_end,
+                        endpoint["price_close"],
+                        endpoint["price_high"],
+                        endpoint["price_low"],
+                        endpoint["price_open"],
+                    ]
+                    df_price.append(df_tmp)
+                    df_price_dict.append([mid_point, np.mean([endpoint["price_high"], endpoint["price_low"]])])
 
-        df_price_clean = pd.DataFrame(
-            df_price, columns=["time_period_start", "time_period_end", "price_close", "price_high", "price_low", "price_open"]
-        )
-        df_price_dict_df = pd.DataFrame(df_price_dict, columns=["time", "price"])
-        df_price_dt = df_price_dict_df.set_index("time").T.to_dict("list")
+            df_price_clean = pd.DataFrame(
+                df_price, columns=["time_period_start", "time_period_end", "price_close", "price_high", "price_low", "price_open"]
+            )
+            df_price_dict_df = pd.DataFrame(df_price_dict, columns=["time", "price"])
+            df_price_dt = df_price_dict_df.set_index("time").T.to_dict("list")
 
-        for ind, row in df_price_clean.iterrows():
-            midpoint = row["time_period_start"] + timedelta(seconds=30)
-            plt.plot([midpoint, midpoint], [row["price_low"], row["price_high"]], linewidth=1, color="k")
-            if row["price_open"] < row["price_close"]:
-                plt.plot([midpoint, midpoint], [row["price_open"], row["price_close"]], linewidth=2, color="r")
-            else:
-                plt.plot([midpoint, midpoint], [row["price_open"], row["price_close"]], linewidth=2, color="b")
+            for ind, row in df_price_clean.iterrows():
+                midpoint = row["time_period_start"] + timedelta(seconds=30)
+                plt.plot([midpoint, midpoint], [row["price_low"], row["price_high"]], linewidth=1, color="k")
+                if row["price_open"] < row["price_close"]:
+                    plt.plot([midpoint, midpoint], [row["price_open"], row["price_close"]], linewidth=2, color="r")
+                else:
+                    plt.plot([midpoint, midpoint], [row["price_open"], row["price_close"]], linewidth=2, color="b")
 
-        ax.grid(which="minor")
-        hourlocator = md.HourLocator()
-        ax.xaxis.set_major_locator(hourlocator)
-        ax.xaxis.set_major_formatter(md.DateFormatter("%H:%M"))
-        ax.xaxis.set_minor_locator(md.MinuteLocator(byminute=range(0, 60, 10)))
-        plt.xlim(min(df_price_clean["time_period_start"]), max(df_price_clean["time_period_end"]))
+            ax.grid(which="minor")
+            hourlocator = md.HourLocator()
+            ax.xaxis.set_major_locator(hourlocator)
+            ax.xaxis.set_major_formatter(md.DateFormatter("%H:%M"))
+            ax.xaxis.set_minor_locator(md.MinuteLocator(byminute=range(0, 60, 10)))
+            plt.xlim(min(df_price_clean["time_period_start"]), max(df_price_clean["time_period_end"]))
 
-        lower_bound = min(df_price_clean["price_low"])
-        upper_bound = max(df_price_clean["price_high"])
+            lower_bound = min(df_price_clean["price_low"])
+            upper_bound = max(df_price_clean["price_high"])
 
-        date_time_marked = datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
-        sc = plt.plot([date_time_marked, date_time_marked], [lower_bound, upper_bound], linestyle="--", color="red", linewidth=0.5)
+            date_time_marked = datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+            sc = plt.plot([date_time_marked, date_time_marked], [lower_bound, upper_bound], linestyle="--", color="red", linewidth=0.5)
 
-        plt.show(block=False)
+            plt.show(block=False)
 
         while True:
             is_influential = input("您认为这条新闻是否是一条对价格有影响力的新闻: \n(y: 非常有影响力，n: 非常没有影响力，s: 不确定): ")
@@ -148,6 +148,7 @@ with open(home + "/coding/annotation/result/result.csv", "w") as csvfile:
 
         writer.writerow({"id": id, "influential": is_influential, "keywords": kws})
 
-        plt.close()
+        if time < "2018-04-14":
+            plt.close()
 
         print("\n")
